77d35bc127033ff1cc35e6c666a6eaabf078aea2
Kate Kim <katehrkim@gmail.com>
2021-06-03 00:00:57 -0400

    Kate's answer

--- python/bubble_sort.py
+++ python/bubble_sort.py
@@ -1,7 +1,20 @@
+def bubble_sort(numbers):
+    swaps = 0
+    index = 0
+    while index + 1 < len(numbers):
+        previous = numbers[index]
+        current = numbers[index + 1]
+        if previous > current:
+            numbers[index] = current
+            numbers[index + 1] = previous
+            swaps += 1
+            index = 0
+        else:
+            index = index + 1
+    return numbers
+
 sequence = [4, 3, 5, 0, 1]
-swaps = 0
+print(bubble_sort(sequence))
 
-# Your Code Here
-
-print(f"Final result: {result}")
-print(f"Swaps: {swaps}")
+# print(f"Final result: {result}")
+# print(f"Swaps: {swaps}")
